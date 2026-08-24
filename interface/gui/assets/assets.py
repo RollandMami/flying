34e0c97f75cb0a8c0153cd36523064d4cd66d874
RollandMami <andrianavalonarolland@gmail.com>
2026-08-24 21:29:11 +0200

    assets py

--- interface/gui/assets/assets.py
+++ interface/gui/assets/assets.py
@@ -1,4 +1,23 @@
 import pygame
 
 
-#DRONE_DEATH =
+def load_spritesheet(path: str,
+					 t_width: int,
+					 t_height: int,
+					 ) -> list[pygame.Surface]:
+	spritesheet = pygame.image.loat(path).convert_alpha()
+	sheet_width, sheet_height = spritesheet.get_size()
+	tiles: list[pygame.Surface] = []
+
+	for col in range(0, sheet_width, t_width):
+		s = pygame.Rect((col, 0),(t_width, t_height))
+		tiles.append(s)
+	return tiles
+
+
+
+DRONE_DEATH = load_spritesheet("./drone_death.png", 120, 100)
+DRONE_WALK = load_spritesheet("./drone_WALK.png", 120, 100)
+DRONE_IDLE = load_spritesheet("./drone_IDLE.png", 120, 80)
+DRONE_SCAN = load_spritesheet("./drone_scan.png", 120, 80)
+
--- interface/gui/widget/base_widget.py
+++ interface/gui/widget/base_widget.py
@@ -1,7 +1,22 @@
 from abc import ABC, abstractmethod
+import pygame
+from typing import Callable
 
 
 class BaseWidget(ABC):
+
+    def __init__(self,
+                text: str,
+                width: int,
+                height: int,
+                font: pygame.font.Font,
+                bg_color: str,
+                font_color: str,
+                master: pygame.Surface,
+                position: tuple[int, int],
+                call: Callable[..., Any],
+                elevation: int = 6) -> None:
+        ...
 
     @abstractmethod
     def draw(self) -> None:
